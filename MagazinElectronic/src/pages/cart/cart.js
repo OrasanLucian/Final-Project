301f4dfb3b477a83d859234901923d9542769d98
Orasan Lucian <lucianraul2003@gmail.com>
2023-06-23 00:47:18 +0300

    cart page updated

--- MagazinElectronic/src/pages/cart/cart.js
+++ MagazinElectronic/src/pages/cart/cart.js
@@ -9,29 +9,47 @@ import {
 const showProducts = async () => {
   const cart = localStorage.getItem("cart");
   const products = JSON.parse(cart);
+  const sortedProducts = products.sort((a, b) => a.id - b.id);
+
   document.getElementById("cart").innerHTML = "";
   let totalSum = 0;
-  products.forEach((product) => {
-    getProductsById(product.id).then((productInfo) => {
-      const totalPrice = productInfo.price * product.quantity;
-      totalSum += totalPrice;
-      document.getElementById("cart").innerHTML += `<tr id="p${product.id}">
+
+  for (const product of sortedProducts) {
+    const productInfo = await getProductsById(product.id);
+    const totalPrice = productInfo.price * product.quantity;
+    totalSum += totalPrice;
+    document.getElementById("cart").innerHTML += `<tr id="p${product.id}">
         <td id="product-name"><a href="/src/pages/details/details.html?id=${
           product.id
         }">${productInfo.name}</a></td>
         <td><img id="products" src=${productInfo.image} /></td>
         <td>${productInfo.price} $</td>
-        <td><button id=${product.id} class="decrement-quantity">-</button> ${
-        product.quantity
-      } <button id=${product.id} class="increment-quantity">+</button></td>
-        <td>${totalPrice.toFixed(2)} $</td>
+        <td><button id=${
+          product.id
+        } class="decrement-quantity">-</button> <span class="quantity">${
+      product.quantity
+    }</span> <button id=${product.id} class="increment-quantity">+</button></td>
+        <td class="subtotal">${totalPrice.toFixed(2)} $</td>
         <td><button class='remove-product'>Remove</button></td>
         </tr>`;
+  }
+  updateTotalSum(sortedProducts);
+};
 
-      document.getElementById("total-price").innerHTML =
-        "TOTAL PRICE: " + totalSum.toFixed(2) + " " + "$";
+const updateTotalSum = async (cartArray) => {
+  let totalSum = 0;
+
+  const productPromises = cartArray.map((product) => {
+    return getProductsById(product.id).then((productInfo) => {
+      const totalPrice = productInfo.price * product.quantity;
+      totalSum += totalPrice;
     });
   });
+
+  await Promise.all(productPromises);
+
+  document.getElementById("total-price").innerHTML =
+    "TOTAL PRICE: " + totalSum.toFixed(2) + " " + "$";
 };
 
 window.addEventListener("load", showProducts);
@@ -42,24 +60,42 @@ document.getElementById("cart").addEventListener("click", async (e) => {
 
   if (e.target.classList.contains("decrement-quantity")) {
     decrementProductQuantity(productId, cartArray);
+    updateTotalSum(cartArray);
   } else if (e.target.classList.contains("increment-quantity")) {
     incrementProductQuantity(productId, cartArray);
+    updateTotalSum(cartArray);
+  } else if (e.target.classList.contains("remove-product")) {
+    var buttonClicked = e.target;
+    buttonClicked.parentElement.parentElement.remove();
+    localStorage.removeItem(cartArray.splice(cartArray.indexOf(productId), 1));
+    updateTotalSum(cartArray);
   }
 
   localStorage.setItem("cart", JSON.stringify(cartArray));
 
-  /*if (!isProductAlreadyInCart(productId, cartArray)) {
-    document
+  if (!isProductAlreadyInCart(productId, cartArray)) {
+    const elementToRemove = document
       .getElementById("cart")
-      .querySelector("#p" + productId)
-      .remove();
+      .querySelector("#p" + productId);
+    if (elementToRemove) {
+      elementToRemove.remove();
+    }
   } else {
-    document
-      .getElementById("cart")
-      .querySelector("#p" + productId)
-      .querySelector(".quantity").innerHTML =
-      getProductQuantityFromLocalStorage(productId, cartArray);
-  }*/
+    const productRow = document.getElementById("p" + productId);
+    const productQuantityElement = productRow.querySelector(".quantity");
+    const productQuantity = getProductQuantityFromLocalStorage(
+      productId,
+      cartArray
+    );
+    productQuantityElement.innerHTML = productQuantity;
+
+    const productInfo = await getProductsById(productId);
+    const totalPrice = productInfo.price * productQuantity;
+    productRow.querySelector("td.subtotal").innerHTML =
+      totalPrice.toFixed(2) + " $";
+
+    updateTotalSum(cartArray);
+  }
 });
 
 const validationMessage = document.getElementById("message");
